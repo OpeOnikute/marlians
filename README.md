ada8b81c548b89feb5161fa71d5e7c37f62901da
OpeOnikute <opeyemionikute@yahoo.com>
2019-10-21 22:40:29 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Marlians! 🤸🏽‍♂️🙅🏽‍♂️
+To keep morale high, this site plays popular chants at intervals.
+
+# Contribute?
+This would be more fun with other people adding their own sounds.
+1. Fork the repository.
+2. Add your sound in `/sounds` with an easy-to-understand name.
+3. Add the name of the sound file to the queue in `js/app.js`.
+4. Commit your changes and create a pull request.
---
+++ js/app.js
@@ -0,0 +1,43 @@
+const queue = ["Gbese.m4a", "Ayiiii.m4a", "Egbe x1.m4a", "Inside Life.m4a", "Egbe x3.m4a", "Kuronbe.m4a", "Marlians.m4a"];
+const interval = 30 * 1000; // 30 seconds
+let timer;
+
+function run () {
+    const curr = queue.shift();
+
+    changeButton("play");
+
+    play(curr);
+    queue.push(curr);
+
+    timer = setTimeout(function() {
+        run();
+    }, interval);
+}
+
+function stopRunning () {
+    changeButton("stop");
+    clearTimeout(timer);
+}
+
+function play(sound) {
+    var audioElem = new Audio(`sounds/${sound}`);
+    audioElem.play();
+}
+
+function changeButton (state) {
+    var playButton = document.getElementById("play-button");
+    var pauseButton = document.getElementById("stop-button");
+    switch(state) {
+        case "play":
+            playButton.style.display = "none";
+            pauseButton.style.display = "inline-block";
+            break;
+        case "stop":
+            playButton.style.display = "inline-block";
+            pauseButton.style.display = "none";
+            break;
+        default:
+            break;
+    }
+}
